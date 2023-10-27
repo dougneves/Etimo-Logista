95506d5331caa930a936798742b1aa221ca5b9b2
Douglas Neves <dougneves@gmail.com>
2023-10-27 18:01:50 +0100

    adiciona efeitos maneiros na hora de arrastar um objeto

--- scripts/ant_colony.gd
+++ scripts/ant_colony.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
 var picked_up = false
+var drop_position = Node2D.new()
 var initial_position = Vector2.ZERO
+var mouse_offset = Vector2.ZERO
+var initial_scale = Vector2.ZERO
 
 func _process(delta):
 	if picked_up:
-		global_position = get_global_mouse_position()
+		global_position = get_global_mouse_position() + mouse_offset
 
 func _on_button_button_down():
 	picked_up = true
+	
+	drop_position = duplicate()
+	drop_position.z_index = 9
+	drop_position.modulate = Color(1, 1, 1, 0.5)
+	get_tree().get_root().add_child(drop_position);
+	
+	z_index = 10
+
+	initial_scale = scale	
+	apply_scale(Vector2(1.1,1.1))
+	mouse_offset = global_position - get_global_mouse_position()
 	initial_position = global_position
 
 func _on_button_button_up():
 	picked_up = false
+	drop_position.queue_free()
+	scale = initial_scale
 	global_position = initial_position
--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -5,6 +5,7 @@ func _on_button_new_ant_colony_pressed():
 	var ant_colony_instance = preload("res://objects/ant_colony.tscn").instantiate()
 	ant_colony_instance.translate(Vector2(randf_range(0,1920),randf_range(0,1080)))
 	ant_colony_instance.rotate(randf_range(0,360))
+	ant_colony_instance.z_index = 8
 	var ant_colory_scale = randf_range(0,1);
 	ant_colony_instance.apply_scale(Vector2(ant_colory_scale,ant_colory_scale))
 	get_tree().get_root().add_child(ant_colony_instance)
